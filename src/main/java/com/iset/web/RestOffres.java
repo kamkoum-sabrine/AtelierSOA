c67069da6f0d3c1d70d39bb5f4f01ec5f62ada7d
Kamkoum Sabrine <kamkoumsabrine30@gmail.com>
2022-10-12 13:21:30 +0200

    adding @PutRequest

--- src/main/java/com/iset/web/RestOffres.java
+++ src/main/java/com/iset/web/RestOffres.java
@@ -1,6 +1,9 @@
 package com.iset.web;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.PostUpdate;
 
 import com.iset.dao.OffreRepository;
 import com.iset.entities.Offre;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +43,20 @@ public class RestOffres {
     @DeleteMapping("/{id}")
     public void deleteoffre(@PathVariable Long id) {
         offreRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Optional<Offre> updateoffre(@RequestBody Offre newOffre, @PathVariable Long id) {
+        return offreRepository.findById(id)
+                .map(offre -> {
+                    offre.setIntitulé(newOffre.getIntitulé());
+                    offre.setPays(newOffre.getPays());
+                    offre.setSociété(newOffre.getSociété());
+                    offre.setSpecialité(newOffre.getSpecialité());
+                    offre.setNbpostes(newOffre.getNbpostes());
+                    return offreRepository.save(offre);
+                });
+
     }
 
 }
